4d55338a26ff3fb944efacb22b825c52b8f70433
tuncay1961 <161307193+tuncay1961@users.noreply.github.com>
2024-03-25 18:16:32 +0300

    Update Java_Project_1.java

--- Java_Project_1.java
+++ Java_Project_1.java
@@ -1,6 +1,8 @@
-public class Java_Project_1{
+import java.util.*;
+public class Dnm2{
    public static void main(String[] args){
         Scanner input=new Scanner(System.in);
+        System.out.println("1) Please enter your birthday between 1 and 7(example Mon=1, Tue=2..):");
         String string_day=input.nextLine();
        
         
@@ -11,7 +13,14 @@ public class Java_Project_1{
         int day=Integer.valueOf(string_day);
         
         //up is for question one--------------------------------------------------------------
-
+      System.out.println("Enter initial letter of your name, (capital english letters only)");
+        String name=input.nextLine();
+        //
+        while(checkingLetter(name)==0){
+            System.out.println("Please enter an appropriate initial letter");
+            name=input.nextLine();
+        };
+//THİS İS FOR İNİTİAL LETTER QUESTİON
         
         input.close();
 
@@ -51,11 +60,41 @@ public class Java_Project_1{
 
         
 
+    };public static int checkingLetter(String name){
+        boolean result1=true;
+        boolean result2=true;
+        boolean result3=true;
+        if(name.length()!=1){
+            result1=false;
+        }
+        else{char first_letter_Ofname=name.charAt(0);
+            if(Character.isLetter(first_letter_Ofname)!=true){
+                result2=false;
+
+            }
+            
+            else{
+                if((first_letter_Ofname>='A' && first_letter_Ofname<='Z')==false){
+                    result3=false;
+
+
+                }
+            }
+        };
+        if((result1 && result2 && result3)==true){
+            return 1;
+        }
+        else{return 0;}
+        
+        
+        
+        
+
     }
 
     
 }
 
 
-}
+
 
