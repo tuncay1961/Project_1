251ee6048106f643f9e89fe27383fe035937c753
tuncay1961 <161307193+tuncay1961@users.noreply.github.com>
2024-03-25 17:37:14 +0300

    Update Java_Project_1.java

--- Java_Project_1.java
+++ Java_Project_1.java
@@ -1,7 +1,61 @@
 public class Java_Project_1{
-    public static void main(String[] args){
-        System.out.println("hello");
+   public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        String string_day=input.nextLine();
+       
+        
+        while(control(string_day)==0){
+            System.out.println("Please enter an appropriate day!");
+            string_day=input.nextLine();
+        };
+        int day=Integer.valueOf(string_day);
+        
+        //up is for question one--------------------------------------------------------------
+
+        
+        input.close();
+
+
+    };
+    public static int control(String string_day){
+        //digit mi değil mi onun true falseu
+        boolean result1=true;
+        for(int i=0;i<(string_day.length());i++){
+            
+            char char0fDay = string_day.charAt(i);
+            boolean check=Character.isDigit(char0fDay);
+            
+            if(check==false){result1=false;break;};
+            
+
+
+        };
+        //aralık kontrolü true falseu
+        boolean result2=true;
+        //eğer false ise direkt return false yapıyor çünkü bu bir kelime
+        if(result1!=false){
+        int day_as_number=Integer.valueOf(string_day);
+        
+        //aralıkta mı değil mi onu kontro eder
+        if(day_as_number<=7 && day_as_number>=1){
+            result2=true;
+        }
+        else{result2=false;};}
+        else{return 0;};
+       
+        if((result1)==false){return 0;}
+        else if(result2==false){return 0;}
+        else{return 1;}
+        
+        
+
+        
+
     }
+
+    
+}
+
 
 }
 
